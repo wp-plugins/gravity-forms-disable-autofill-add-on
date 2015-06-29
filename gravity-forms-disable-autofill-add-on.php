6e3d6c00e537561531f3dd8f3db3fba1bd87fc44
amg26 <amg26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 11:00:45 +0000

    udatea
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-disable-autofill-add-on/trunk@1189461 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravity-forms-disable-autofill-add-on.php
+++ gravity-forms-disable-autofill-add-on.php
@@ -3,7 +3,7 @@
 * Plugin Name: Gravity Forms Disable Autofill Add-On
 * Plugin URI: http://andrewmgunn.com/gravity-forms-disable-autofill-add-on/
 * Description: Disable the browser's ability to autofill forms and input fields on selected Gravity Forms. Ideal for forms with sensitive information and provides extra level of form submission security.
-* Version: 1.1
+* Version: 1.3
 * Author: Andrew M. Gunn
 * Author URI: http://andrewmgunn.com
 * Text Domain: gravity-forms-disable-autofill-add-on
@@ -11,6 +11,24 @@
 */
 
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+include('options/options.php');
+
+add_action( 'admin_init', 'require_gravity_forms' );
+
+function require_gravity_forms() {
+    if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'gravityforms/gravityforms.php' ) ) {
+        add_action( 'admin_notices', 'add_on_plugin_notice' );
+
+        deactivate_plugins( plugin_basename( __FILE__ ) );
+
+        if ( isset( $_GET['activate'] ) ) {
+            unset( $_GET['activate'] );
+        }
+    }
+}
+function add_on_plugin_notice() {
+	echo '<div class="error"><p>Gravity Forms must be installed and activated to use this plugin!</p></div>';
+}
 
 add_action( 'wp_enqueue_scripts', 'register_gfdaa_scripts' );
 
